8e23e1027abf9305ddeca3ed357d307cbeafda6a
georgina puig <georginaxcodes@gmail.com>
2020-08-05 18:58:04 -0500

    validate data

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,37 @@ const User = require('../model/User');
 // validation
 const Joi = require('@hapi/joi');
 
+const schema = Joi.object({
+  name: Joi.string()
+    .min(3)
+    .required(),
+  email: Joi.string()
+    .min(6)
+    .required()
+    .email(),
+  phone: Joi.string()
+    .min(6)
+    .required(),
+  password: Joi.string()
+    .min(6)
+    .required(),
+  age: Joi.number()
+    .integer()
+    .min(18)
+    .max(65)
+    .required(),
+  gender: Joi.string()
+    .valid('female', 'male'),
+  hobby: Joi.string()
+    .min(3)
+    .required(),
+});
+
 router.post('/register', async (req, res) => {
+  // validate data
+  const {error} = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
