9bece73a5adf63801eb84847c0448b1d46876e42
georgina puig <georginaxcodes@gmail.com>
2020-08-05 00:04:21 -0500

    import register post route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
-
+// import routes
+const authRoute = require('./routes/auth');
 
 app.listen(3000, () => console.log(`The server has started in port 3000 - http://localhost:3000/`));
