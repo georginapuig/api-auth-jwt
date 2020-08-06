e8797e39e0c48395267ef4e1afd9cc544aecf39d
georgina puig <georginaxcodes@gmail.com>
2020-08-05 19:26:09 -0500

    check if email already exist

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,11 @@ router.post('/register', async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // check id the user is already in the db
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('Email already exist');
+
+  // create new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
